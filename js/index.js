d912f4c0e955968ddebf3b05bf5dd2184affee96
MeetShah2003 <meetshah21102003@gmail.com>
2023-08-23 17:44:39 +0530

    Add to Cart feature added

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ document.getElementById("navbar").innerHTML = nav();
 
 let products=[];
 
-const display = ((products1) => {
+const display = (products1) => {
   document.getElementById("right-box").innerHTML="";
     products1.map((item) => {
     let img = document.createElement("img");
@@ -18,26 +18,56 @@ const display = ((products1) => {
     let desc = document.createElement("p");
     desc.innerHTML = item.description;
     let div=document.createElement("div");
-    // let rating=document.createElement("h5");
-    // if(rating.item.rating.rate>4){
-    //     rating.innerHTML="****";
-    // }
-    // else if(rating.item.rating.rate<=3&&rating.item.rating.rate>=4){
-    //     rating.innerHTML="***";
-    // }
-    // else{
-    //     rating.innerHTML="**";
-    // }
+    let rating=document.createElement("h5");
+    if(item.rating.rate>4){
+        rating.innerHTML="****";
+    }
+    else if(item.rating.rate<=3&&item.rating.rate>=4){
+        rating.innerHTML="***";
+    }
+    else{
+        rating.innerHTML="**";
+    }
     let btn1=document.createElement("button");
     btn1.innerHTML="Buy now";
     btn1.setAttribute("id","buy");
     let btn2=document.createElement("button");
     btn2.innerHTML="Add to Cart";
     btn2.setAttribute("id","cart");
-    div.append(img,title,cat,price,desc,btn1,btn2);
+    btn2.addEventListener("click",()=>{
+      let login = localStorage.getItem("login")
+            if(login){
+                    fetch(`http://localhost:1020/cart/?id=${item.id}`)
+                    .then((res)=>res.json())
+                    .then((data)=>{
+                        if(data.length > 0){
+                            data[0].qty = data[0].qty+1
+                            console.log(data[0]);
+                            fetch(`http://localhost:1020/cart/${data[0].id}`,{
+                                method:"PATCH",
+                                headers:{"Content-type":"application/json"},
+                                body:JSON.stringify(...data)
+                            })  
+                        }
+                        else{
+                            fetch(`http://localhost:1020/cart`,{
+                                method:"POST",
+                                headers:{"Content-type":"application/json"},
+                                body:JSON.stringify({...item,qty:1})
+                            })
+                        }
+                    })
+    
+            }   
+            else{
+                window.location.href="../pages/login.html"
+            }
+        // })
+    });
+    div.append(img,title,cat,price,desc,rating,btn1,btn2);
     document.getElementById("right-box").append(div);
   });
-});
+};
 
 
 const get = () => {
